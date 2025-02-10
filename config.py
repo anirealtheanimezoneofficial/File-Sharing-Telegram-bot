e84b9193db52198446f1970e2cd74388093c8405
anirealtheanimezoneofficial <anirealtheanimezoneofficial@gmail.com>
2025-02-10 22:53:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ USER_REPLY_TEXT = os.environ.get("USER_REPLY_TEXT", "Don't send me messages dire
 #your bot token here from https://telegram.me/BotFather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "") 
 #your api id from https://my.telegram.org/apps
-APP_ID = int(os.environ.get("APP_ID", "7009167334"))
+APP_ID = int(os.environ.get("APP_ID", "22660802"))
 #your api hash from https://my.telegram.org/apps
 API_HASH = os.environ.get("API_HASH", "fec849af712bc23ab5f18277b1592d2e")
 #your channel_id from https://t.me/MissRose_bot by forwarding dummy message to rose and applying command `/id` in reply to that message
